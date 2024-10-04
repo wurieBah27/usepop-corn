df453c247b53f3e028468af4854e46c877d41921
wurieBah27 <wuriebah27@gmail.com>
2024-10-04 14:46:13 +0400

    hdhh

--- src/components/Results/WatchedMoviesSummary.jsx
+++ src/components/Results/WatchedMoviesSummary.jsx
@@ -4,6 +4,7 @@ const WatchedMoviesSummary = ({ watched, average }) => {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const trye = true;
 
   return (
     <div className="summary">
